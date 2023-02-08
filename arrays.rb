b2e593454fe961169fd34163d87b8cca9d8e763b
Alex Kiselich <alexander.kiselich@gmail.com>
2023-02-07 23:31:46 -0700

    add a line of code that uses index positions to pull out of the arrays

--- arrays.rb
+++ arrays.rb
@@ -1,5 +1,7 @@
 names = ["Leonardo", "Raphael", "Donatello", "Michelangelo"]
 numbers = [1, 2, 3, 4]
-flaots = [1.1, 2.2, 3.3, 4.4]
+floats = [1.1, 2.2, 3.3, 4.4]
 booleans = [true, false, true, false]
- 
+
+# This line of code is pulling the elements in the array based on the index position provided. 
+puts names[2], numbers[3], floats[0], booleans[1]
